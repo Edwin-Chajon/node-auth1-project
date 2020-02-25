33a5ba27b12992f365b6f482c3960c1907bc6bdc
Edwin Chajon <junchajo@gmail.com>
2020-02-25 12:12:09 -0800

    part 2

---
+++ auth/restricted.js
@@ -0,0 +1,21 @@
+const bcrypt = require("bcryptjs");
+
+const Users = require("../users/model");
+
+module.exports = (req, res, next) => {
+  let { username, password } = req.headers;
+
+  username && password ?
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        user && bcrypt.compareSync(password, user.password) ?
+          next()
+        : res.status(401).json({ message: "Invalid Credentials" });
+        
+      })
+      .catch(({ name, message, stack }) => {res.status(500).json({ name, message, stack })})
+    :
+    res.status(400).json({ error: "please provide credentials" });
+  
+};
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const server = express();
 
+const apiRouter = require('./api/api_router');
+
 server.use(express.json());
 
-server.get("/", (req, res) => {res.send('this is a thing :p')});
+server.use('/api', apiRouter);
 
 module.exports = server;
